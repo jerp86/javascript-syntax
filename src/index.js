3ecfb8f0d00c7d47f420f28f62e5756b457498f9
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-20 06:52:07 -0300

    finishin tests to employee

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ const GENDER = {
   assert.throws(() => employee.birthYear, { message: 'you must define age first!!' });
 }
 
+const CURRENT_YEAR = 2021;
+Date.prototype.getFullYear = () => CURRENT_YEAR;
+
 {
   const employee = new Employee({
     name: 'Joaozinho',
@@ -28,4 +31,21 @@ const GENDER = {
   assert.deepStrictEqual(employee.gender, undefined);
   assert.deepStrictEqual(employee.grossPay, Util.formatCurrency(5000.40));
   assert.deepStrictEqual(employee.netPay, Util.formatCurrency(4000.32));
+
+  const expectedBirthYear = 2001
+  assert.deepStrictEqual(employee.birthYear, expectedBirthYear);
+
+  // não tem set, não vai mudar!
+  employee.birthYear = new Date().getFullYear() - 80;
+  assert.deepStrictEqual(employee.birthYear, expectedBirthYear);
+
+  employee.age = 80;
+  assert.deepStrictEqual(employee.birthYear, 1941);
+
+  console.log('\n ----- employee -----');
+  console.log('employee.name', employee.name);
+  console.log('employee.age', employee.age);
+  console.log('employee.gender', employee.gender);
+  console.log('employee.grossPay', employee.grossPay);
+  console.log('employee.netPay', employee.netPay);
 }
