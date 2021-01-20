19d086139df0252f701063945da57362c7e82300
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-20 07:05:01 -0300

    refactory netPay in Manager

--- src/manager.js
+++ src/manager.js
@@ -7,6 +7,13 @@ class Manager extends Employee {
   get bonuses() {
     return Util.formatCurrency(this.#bonuses);
   }
+
+  get netPay() {
+    const finalValue =
+      Util.unFormatCurrency(super.netPay) + Util.unFormatCurrency(this.bonuses);
+
+    return Util.formatCurrency(finalValue);
+  }
 }
 
 module.exports = Manager;
