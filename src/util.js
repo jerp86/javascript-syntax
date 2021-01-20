0656f5fe1a9adb4bfa8c85c0e6520710d3cc69f5
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-20 07:04:24 -0300

    add unFormat in Util

--- src/util.js
+++ src/util.js
@@ -7,6 +7,10 @@ class Util {
   static formatCurrency(value) {
     return this.#defaultFormat.format(value);
   }
+
+  static unFormatCurrency(value) {
+    return Number(value.replace(/\D/g, '')) / 100
+  }
 }
 
 module.exports = Util;
